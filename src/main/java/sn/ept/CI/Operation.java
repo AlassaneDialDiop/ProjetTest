dedb0d686d10c8bfa9c844ccc5691ae5c3105bcb
AlassaneDiop <diopad@ept.sn>
2018-02-24 11:14:47 +0000

    ajout de la methode sub

--- src/main/java/sn/ept/CI/Operation.java
+++ src/main/java/sn/ept/CI/Operation.java
@@ -9,22 +9,34 @@ public class Operation
 	* @return a+b the sum of a and b
 	*/
 	public int add(int a, int b) {
-		return a +b;
+		return a+b;
 	}
 	
 	public double add(double a, double b) {
 		return a + b;
 	}
 /**
+	*This method computes the diff of two double values
+	*@param a first double value
+	* @param b second double value
+	* @return a+b the product of a and b
+	*/
+    public int sub(int a, int b) {
+    	return a-b;
+    }
+    public double sub(double a, double b) {
+    	return a-b;
+    }
+/**
 	*This method computes the sum of two double values
 	*@param a first double value
 	* @param b second double value
-	* @return a+b the sum of a and b
+	* @return a+b the product of a and b
 	*/
     public int multiply(int a, int b) {
     	return a*b;
     }
-    public double multiply(int a, int b) {
+    public double multiply(double a, double b) {
     	return a*b;
     }
 }
